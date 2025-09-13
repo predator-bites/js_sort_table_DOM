99c4f037ee12dc4d553dd44b093d31c68981401e
DropPredator <ionchiccc@gmail.com>
2025-09-13 10:48:16 +0300

    ready again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,17 +5,22 @@ const tbody = document.querySelector('tbody');
 const headers = document.querySelector('thead tr');
 
 headers.addEventListener('click', (e) => {
-  const childs = [...headers.children];
   const listToSort = [];
-  const aimIndx = childs.findIndex((elem, i) => {
-    if (elem === e.target) {
-      return true;
+
+  const th = e.target.closest('th');
+  const aimIndx = th?.cellIndex;
+
+  if (aimIndx === -1) {
+    return;
+  }
+
+  for (let i = 0; i < bodyRows.length; i++) {
+    const row = bodyRows[i];
+
+    if (row.children.length < aimIndx) {
+      return;
     }
 
-    return false;
-  });
-
-  for (const row of bodyRows) {
     listToSort.push({
       aimCell: row.children[aimIndx],
       row: row,
@@ -23,13 +28,24 @@ headers.addEventListener('click', (e) => {
   }
 
   listToSort.sort((elem1, elem2) => {
+    if (
+      typeof elem1 === 'undefined' ||
+      elem1 === null ||
+      typeof elem2 === 'undefined' ||
+      elem2 === null
+    ) {
+      return;
+    }
+
     if (aimIndx === 0 || aimIndx === 1) {
-      return elem1.aimCell.textContent.localeCompare(elem2.aimCell.textContent);
+      return elem1.aimCell.textContent
+        .toLowerCase()
+        .localeCompare(elem2.aimCell.textContent.toLowerCase());
     } else if (aimIndx === 2) {
       return +elem1.aimCell.textContent - +elem2.aimCell.textContent;
     } else if (aimIndx === 3) {
-      const newElem1 = Number(elem1.aimCell.textContent.replaceAll(/\D/g, ''));
-      const newElem2 = Number(elem2.aimCell.textContent.replaceAll(/\D/g, ''));
+      const newElem1 = Number(elem1.aimCell.textContent.replace(/\D/g, ''));
+      const newElem2 = Number(elem2.aimCell.textContent.replace(/\D/g, ''));
 
       return newElem1 - newElem2;
     }
